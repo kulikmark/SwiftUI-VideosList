2d3c5ad22afba0a7c70cc24fcd2e234d4a9b376f
Mark Kulik <kulik_mark@mail.ru>
2022-02-05 23:16:33 +0300

    Initial Commit

---
+++ SwiftUI-VideosList/SwiftUI_VideosListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_VideosListApp.swift
+//  SwiftUI-VideosList
+//
+//  Created by Марк Кулик on 05.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_VideosListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
